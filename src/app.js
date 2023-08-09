09f2feaf330ef2e944b3a78dc20a1d3c4b30a54a
Allan Chaves <allancac@gmail.com>
2023-08-08 23:10:18 -0300

    Add basic middlewares
    - Add Json Body Parser middelware
    - Add URL encoded middleware
    - Add CORS middleware
    - Add Morgan Middleware to log all requests

--- src/app.js
+++ src/app.js
@@ -1,18 +1,37 @@
+import config from 'config'
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 class App {
     constructor(){
         this.app = express();
-        this.routes();
         this.middlewares();
+        this.routes();
         return this.app;
     }
-
-    routes(){
-
-    }
+    
     middlewares(){
+        // Body parser Middleware to receive a JSON data in the body 
+        this.app.use(express.json());
 
+        // Middleware for encoded
+        this.app.use(express.urlencoded({ extended: true }));
+
+        // CORS - Cross Origin Resource Sharing Middleware configuration
+        this.app.use(cors({
+            origin: config.get('origin.host'),
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            allowedHeaders: 'Authorization, Origin, X-Requested-With, Content-Type, Accept',
+        }));
+
+        // Morgan middleware - Logs requests in the development environment
+        if (process.env.NODE_ENV === 'development') {
+            this.app.use(morgan('dev'))
+        }
+    }
+    
+    routes(){
     }
 }
 
