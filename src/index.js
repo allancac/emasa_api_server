2719efef1846f320b70767b0aa943cd701ae2801
Allan Chaves <allancac@gmail.com>
2023-08-10 01:25:04 -0300

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import config from 'config';
-import app from './app' 
+import config from "config";
+import app from "./app";
+const port = config.get("server.port") || 5500;
 
-const port = config.get('server.port') || 5500;
+const startServer = async () => {
+  app.listen(port, () => {
+    console.log(`Express Server is running on port ${port} `);
+  });
+};
 
-app.listen(port, ()=>{
-    console.log(`Express Server is running on port ${port} `)
-})
+startServer();
