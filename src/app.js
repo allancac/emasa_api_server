d0684040d644b515a7bf48705e488e34189ec462
Allan Chaves <allancac@gmail.com>
2023-08-10 01:22:12 -0300

    Fix app.js - Correct format - Correct constructor method

--- src/app.js
+++ src/app.js
@@ -1,40 +1,42 @@
-import config from 'config'
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import statusRoutes from './routes/statusRoutes';
+import config from "config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import statusRoutes from "./routes/statusRoutes";
 
 class App {
-    constructor(){
-        this.app = express();
-        this.middlewares();
-        this.routes();
-        return this.app;
-    }
-    
-    middlewares(){
-        // Body parser Middleware to receive a JSON data in the body 
-        this.app.use(express.json());
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
 
-        // Middleware for encoded
-        this.app.use(express.urlencoded({ extended: true }));
+  middlewares() {
+    // Body parser Middleware to receive a JSON data in the body
+    this.app.use(express.json());
 
-        // CORS - Cross Origin Resource Sharing Middleware configuration
-        this.app.use(cors({
-            origin: config.get('origin.host'),
-            methods: ["GET", "POST", "PUT", "DELETE"],
-            allowedHeaders: 'Authorization, Origin, X-Requested-With, Content-Type, Accept',
-        }));
+    // Middleware for encoded
+    this.app.use(express.urlencoded({ extended: true }));
 
-        // Morgan middleware - Logs requests in the development environment
-        if (process.env.NODE_ENV === 'development') {
-            this.app.use(morgan('dev'))
-        }
+    // CORS - Cross Origin Resource Sharing Middleware configuration
+    this.app.use(
+      cors({
+        origin: config.get("origin.host"),
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders:
+          "Authorization, Origin, X-Requested-With, Content-Type, Accept",
+      })
+    );
+
+    // Morgan middleware - Logs requests in the development environment
+    if (process.env.NODE_ENV === "development") {
+      this.app.use(morgan("dev"));
     }
-    
-    routes(){
-        this.app.use([statusRoutes] )
-    }
+  }
+
+  routes() {
+    this.app.use([statusRoutes]);
+  }
 }
 
-export default new App();
+export default new App().app;
