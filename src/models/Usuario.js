2a8183e2dbe7b65f1ed6234c744ada0be2c1f938
Allan Cezar Almeida Chaves <allancac@gmail.com>
2023-08-24 02:03:30 -0300

    Fix Usuario Model

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,17 +1,24 @@
-const { Sequelize, DataTypes} = require ('sequelize');
-
-const Usuario=(sequelize)=>{
-	return sequelize.define('Usuario',{
-		id: {type:Sequelize.UUID, primaryKey:true},
-		nomeUsuario: DataTypes.STRING(50),
-		nome: DataTypes.STRING(50),
-		sobrenome: DataTypes.STRING(50),
-		foto: DataTypes.STRING(200),
-		ativo: DataTypes.BOOLEAN,
-		email: DataTypes.STRING(50),
-		senha: DataTypes.STRING(100),
-		perfil: DataTypes.ENUM('vendedor', 'gerente', 'administrador'),
-	})
+const { Sequelize, DataTypes, Model } = require("sequelize");
+class Usuario extends Model{
+  static init(sequelize) {
+    super.init(
+      {
+        id: { type: Sequelize.UUID, primaryKey: true },
+        nomeUsuario: DataTypes.STRING(50),
+        nome: DataTypes.STRING(50),
+        sobrenome: DataTypes.STRING(50),
+        foto: DataTypes.STRING(200),
+        ativo: DataTypes.BOOLEAN,
+        email: DataTypes.STRING(50),
+        senha: DataTypes.STRING(100),
+        perfil: DataTypes.ENUM("vendedor", "gerente", "administrador"),
+      },
+      {
+        sequelize,
+        timestamps: true,
+      }
+    );
+  }
 }
 
 module.exports = Usuario;
